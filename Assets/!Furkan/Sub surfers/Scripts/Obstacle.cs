a52a0679d1026b5bd77324a572351eda74c79205
ozgurd5 <ozgurdalbeler@gmail.com>
2024-03-17 15:30:43 +0300

    Level Edits

--- Assets/!Furkan/Sub surfers/Scripts/Obstacle.cs
+++ Assets/!Furkan/Sub surfers/Scripts/Obstacle.cs
@@ -4,9 +4,19 @@ namespace _Furkan.Sub_surfers.Scripts
 {
     public class Obstacle : MonoBehaviour
     {
+        [Header("References")]
+        [SerializeField] Obstacle linkedObstacle;
+        [SerializeField] private ParticleSystem[] particles;
+        [SerializeField] private MeshRenderer meshRenderer;
+
         public void DestroyObstacle()
         {
-            Destroy(gameObject);
+            linkedObstacle.DestroyObstacle();
+
+            foreach (var particle in particles) particle.gameObject.SetActive(true);
+
+            meshRenderer.enabled = false;
+            Destroy(gameObject, 5f);
         }
     }
 }

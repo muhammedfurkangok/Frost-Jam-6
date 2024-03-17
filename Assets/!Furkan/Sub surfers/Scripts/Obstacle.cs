60dc5bc71d5cd757f93c0d7f8e041e172fc53287
ozgurd5 <ozgurdalbeler@gmail.com>
2024-03-17 14:38:45 +0300

    Obstacle Logic

---
+++ Assets/!Furkan/Sub surfers/Scripts/Obstacle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _Furkan.Sub_surfers.Scripts
+{
+    public class Obstacle : MonoBehaviour
+    {
+        public void DestroyObstacle()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
